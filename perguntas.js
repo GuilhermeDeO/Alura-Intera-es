40bf604794fea8f190ab36ecb19584887a89d1bd
Guilehkk <169073547+GuilhermeDeO@users.noreply.github.com>
2024-11-25 12:37:26 -0300

    Update

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,5 @@
 criaCartao(
-    'História',
+    'Origem',
     'Você sabe a origem do grupo das baleias e golfinhos?',
     'Se originaram de um grupo de mamíferos que se adaptaram à viver em ambientes aquáticos, pelas mudanças de clima e falta de alimento'
 )
@@ -11,13 +11,13 @@ criaCartao(
 )
 
 criaCartao(
-    'Geografia',
-    'Por onde estas espécies estão espalhadas?',
+    'Território',
+    'Por onde essas espécies estão espalhadas?',
     'Praticamente por todo o globo, inclusive nas águas doces das regiões sul, como é o caso dos botos. Algumas espécies realizam migrações anuais, e algumas passam pelos litorais brasileiros'
 )
 
 criaCartao(
-    'A',
-    'A?',
-    'A'
+    'Preservação',
+    'Como está a situação de vida desses animais?',
+    'Após anos de caça ilegal, muitas espécies entraram na faixa de risco, mas com a proibição da caça, os números de suas populações tem crescido cada vez mais'
 )
