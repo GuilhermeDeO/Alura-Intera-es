536c4514adb1b9ae521cf6a2c055ce16b61a626f
Guilehkk <169073547+GuilhermeDeO@users.noreply.github.com>
2024-11-25 10:57:36 -0300

    Update

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
     'História',
     'Você sabe a origem do grupo das baleias e golfinhos?',
-    'Se originaram de um grupo de mamíferos que se adaptaram à viver em ambientes aquáticos'
+    'Se originaram de um grupo de mamíferos que se adaptaram à viver em ambientes aquáticos, pelas mudanças de clima e falta de alimento'
 )
 
 criaCartao(
