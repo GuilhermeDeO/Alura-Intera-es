9a8fedc812fc0fc041ac080d478360b2071daf10
Guilehkk <169073547+GuilhermeDeO@users.noreply.github.com>
2024-11-25 12:42:47 -0300

    Update

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Origem',
+    '🧬 Origem',
     'Você sabe a origem do grupo das baleias e golfinhos?',
     'Se originaram de um grupo de mamíferos que se adaptaram à viver em ambientes aquáticos, pelas mudanças de clima e falta de alimento'
 )
 
 criaCartao(
-    'Curiosidades',
+    '🐋 Curiosidades',
     'Sabia que possuem similaridade com as vacas?',
     'Ambas as espécies fazem parte do grupo dos "ungulados", ou seja, animais que possuem cascos, e seu antepassados possuíam essa características'
 )
 
 criaCartao(
-    'Território',
+    '🌎 Território',
     'Por onde essas espécies estão espalhadas?',
     'Praticamente por todo o globo, inclusive nas águas doces das regiões sul, como é o caso dos botos. Algumas espécies realizam migrações anuais, e algumas passam pelos litorais brasileiros'
 )
 
 criaCartao(
-    'Preservação',
+    '⚕️ Preservação',
     'Como está a situação de vida desses animais?',
     'Após anos de caça ilegal, muitas espécies entraram na faixa de risco, mas com a proibição da caça, os números de suas populações tem crescido cada vez mais'
 )
